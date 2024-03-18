93c826588c3a7c46bfb849dcab77b50e86137bc8
HNY_Badger <markelov-02@list.ru>
2024-03-18 20:08:40 +0400

    feat: finish memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -116,8 +116,14 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let mem;
+  return () => {
+    if (!mem) {
+      mem = func();
+    }
+    return mem;
+  };
 }
 
 /**
